d96b27bd1f53566dca34fb2cd197f1ffca683ef1
jackcrazy <s.zhao.j@gmail.com>
2013-11-01 01:51:03 -0500

    simple method

--- single_method.rb
+++ single_method.rb
@@ -1,7 +1,15 @@
 #!/usr/bin/env ruby
 
-def hello
-	puts "hello, this is hello method"
+def hello(name="default")
+	puts "hello #{name}, this is hello method"
 end
 
 hello
+hello("jack")
+
+def world(name1, name2)
+	puts "hello #{name1}"
+	puts "hello #{name2}"
+end
+
+world("Jack", "xxxx")
